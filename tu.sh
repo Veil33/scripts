18d570457240870df83a34099174e1a1c683bf07
Veil33 <54200353+Veil33@users.noreply.github.com>
2024-08-22 20:03:50 +0800

    Update tu.sh

--- tu.sh
+++ tu.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #export UUID=${UUID:-'fc44fe6a-f083-4591-9c03-f8d61dc3907f'}   # 若需要固定UUID，删除改行开头的#，删除第二行
-export HOST_IP=${HOST_IP:-$(HOST_IP)}
-export PASSWORD=${PASSWORD:-$(openssl rand -base64 16)}  # 随机生成password，无需更改
 export UUID=$(openssl rand -hex 16 | awk '{print substr($0,1,8)"-"substr($0,9,4)"-"substr($0,13,4)"-"substr($0,17,4)"-"substr($0,21,12)}')
 export PASSWORD=${PASSWORD:-$(openssl rand -base64 16)}  # 随机生成password，无需更改
 export PORT="${PORT:-${PORT:-1234}}"                   # TUIC 端口必填，改为开放的udp端口
@@ -10,6 +8,9 @@ export NEZHA_PORT=${NEZHA_PORT:-'5555'}             # 哪吒客户端端口为{4
 export NEZHA_KEY=${NEZHA_KEY:-''}                 # 哪吒客户端密钥
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
+
+[[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="domains/${USERNAME}.ct8.pl/logs" || WORKDIR="domains/${USERNAME}.serv00.net/logs"
+[ -d "$WORKDIR" ] || (mkdir -p "$WORKDIR" && chmod 777 "$WORKDIR" && cd "$WORKDIR")
 
 # Download Dependency Files
 ARCH=$(uname -m) && DOWNLOAD_DIR="." && mkdir -p "$DOWNLOAD_DIR" && FILE_INFO=()
@@ -90,6 +91,18 @@ run() {
 run
 
 # get ip
+ipv4=$(curl -s ipv4.ip.sb)
+if [ -n "$ipv4" ]; then
+    HOST_IP="$ipv4"
+else
+    ipv6=$(curl -s --max-time 1 ipv6.ip.sb)
+    if [ -n "$ipv6" ]; then
+        HOST_IP="$ipv6"
+    else
+        echo -e "\e[1;35m无法获取IPv4或IPv6地址\033[0m"
+        exit 1
+    fi
+fi
 echo -e "\e[1;32m本机IP: $HOST_IP\033[0m"
 
 # get ipinfo
@@ -101,5 +114,8 @@ echo ""
 echo -e "\e[1;33mV2rayN 或 Nekobox，跳过证书验证需设置为true\033[0m\n"
 echo -e "\e[1;32mtuic://$UUID:$PASSWORD@$HOST_IP:$PORT?congestion_control=bbr&alpn=h3&sni=www.bing.com&udp_relay_mode=native&allow_insecure=1#$ISP\e[0m"
 echo ""
+
+# delete files
+rm -rf npm web config.json
 
 exit 0
